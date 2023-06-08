1c9829985bc2da6fe37f50a59d2b2159157c3847
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-06-08 15:59:23 -0400

    Added portraits to events

--- SoS Portrait Mod/Plugin.cs
+++ SoS Portrait Mod/Plugin.cs
@@ -41,32 +41,19 @@ public class Plugin : BasePlugin
                 var charName = __instance.gameObject.transform.GetChild(3).GetChild(0).gameObject
                     .GetComponent<UITextMeshProOneLine>();
 
+
                 var checkPortrait = __instance.transform.parent.Find("CharPortrait")?.gameObject;
                 if (checkPortrait != null)
                 {
                     // Set to false in case sprite isn't valid and it was true before
                     var portrait = checkPortrait.GetComponent<Image>();
-                    portrait.sprite = ChangeAssets(charName);
+                    portrait.sprite = ChangeAssets(charName.text);
 
                     checkPortrait.active = portrait.sprite != null;
                 }
                 else
                 {
-                    var gameObject = new GameObject();
-                    var portrait = gameObject.AddComponent<Image>();
-                    var mask = gameObject.AddComponent<Mask>();
-
-                    var transform = portrait.transform;
-                    transform.parent = __instance.transform.parent.transform;
-                    portrait.name = "CharPortrait";
-
-                    transform.position = new Vector3(2003.3f, -0.1964f, 100);
-                    transform.localScale = new Vector3(4.2873f, 4, 108);
-
-                    transform.SetAsFirstSibling();
-
-                    portrait.sprite = ChangeAssets(charName);
-                    gameObject.active = portrait.sprite != null;
+                    CreatePortrait(__instance, charName.text);
                 }
             }
             catch (Exception e)
@@ -75,11 +62,43 @@ public class Plugin : BasePlugin
             }
         }
 
+        [HarmonyPatch(typeof(UIEventMessageManager), "SetFeedMessageAnimation")]
+        [HarmonyPostfix]
+        public static void EventPortraitPatch(UIEventMessageManager __instance)
+        {
+            var charName = __instance.transform.GetChild(4).GetChild(0).gameObject.GetComponent<UITextMeshProOneLine>();
+            var checkPortrait = __instance.transform.parent.Find("CharPortrait")?.gameObject;
+            
+            if (checkPortrait != null)
+            {
+                // Set to false in case sprite isn't valid and it was true before
+                var portrait = checkPortrait.GetComponent<Image>();
+                portrait.sprite = ChangeAssets(charName.text);
+
+                checkPortrait.active = portrait.sprite != null;
+            }
+            else
+            {
+                CreatePortrait(__instance, charName.text);
+            }
+        }
+
+        [HarmonyPatch(typeof(UIEventMessageManager), "CloseMessage")]
+        [HarmonyPostfix]
+        public static void CloseEventPatch(UIEventMessageManager __instance)
+        {
+            var checkPortrait = __instance.transform.parent.Find("CharPortrait")?.gameObject;
+            if (checkPortrait != null)
+            {
+                checkPortrait.active = false;
+            }
+        }
 
         [HarmonyPatch(typeof(PresentMasterData), "CheckPreference")]
         [HarmonyPostfix]
         public static void PresentPortraitPatch(PresentMasterData.PreferenceLevel __result, uint __0)
         {
+            // TODO: check for character id to get name
             // __0 == character id
             var character = MasterDataManager.Instance.CharacterMaster.GetMasterDataByNameId(__0);
             var dlcCharacter = MasterDataManager.Instance.DlcCharacterMaster.GetMasterDataByNameTextId(__0);
@@ -105,12 +124,35 @@ public class Plugin : BasePlugin
             }
         }
 
-        private static Sprite ChangeAssets(UITextMeshProOneLine pText, string extra = "")
-        { 
+        private static void CreatePortrait(Component uiMessage, string charName)
+        {
+            var gameObject = new GameObject();
+            var portrait = gameObject.AddComponent<Image>();
+            var mask = gameObject.AddComponent<Mask>();
+            mask.name = "CharMask";
+
+            var transform = portrait.transform;
+
+            if (uiMessage.name == "EventMessage")
+                transform.parent = uiMessage.transform.parent.parent;
+            transform.parent = uiMessage.transform.parent.transform;
+            portrait.name = "CharPortrait";
+
+            transform.position = new Vector3(2003.3f, -0.1964f, 100);
+            transform.localScale = new Vector3(4.2873f, 4, 108);
+
+            transform.SetAsFirstSibling();
+
+            portrait.sprite = ChangeAssets(charName);
+            gameObject.active = portrait.sprite != null;
+        }
+
+        private static Sprite ChangeAssets(string pText, string extra = "")
+        {
             // help from https://forum.unity.com/threads/generating-sprites-dynamically-from-png-or-jpeg-files-in-c.343735/
             Texture2D texture2D;
             byte[] fileBytes;
-            var filePath = Path.Combine(SpritePath, pText.text + extra + ".png");
+            var filePath = Path.Combine(SpritePath, pText + extra + ".png");
 
             if (!File.Exists(filePath)) return null;
             // Read all bytes from file and convert to sprite and add to dictionary for easier loading in dialog
@@ -119,7 +161,6 @@ public class Plugin : BasePlugin
             return !texture2D.LoadImage(fileBytes)
                 ? null
                 : Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.zero);
-
         }
     }
 }
