9816a1996665b08a9220f7a0e68a39f9173d70af
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-06-08 02:22:12 -0400

    Fixed the freeze time inside and works perfect now

--- SoS Time Changer/Plugin.cs
+++ SoS Time Changer/Plugin.cs
@@ -5,7 +5,6 @@ using BepInEx.Unity.IL2CPP;
 using BokuMono;
 using HarmonyLib;
 using Il2CppSystem.Collections.Generic;
-using UnityEngine.Playables;
 
 namespace SoS_Time_Changer;
 
@@ -29,7 +28,7 @@ public class Plugin : BasePlugin
         list.Add(20300);
         // Beanstalk Island
         list.Add(20400);
-        
+
         // DLC Areas
         // Terracotta Oasis
         list.Add(21212);
@@ -42,11 +41,12 @@ public class Plugin : BasePlugin
 
     public override void Load()
     {
-        _timeScale = Config.Bind("General", "TimeScale", 60, "The speed of time in the game(60 = 1 in game minute per second & 30 = 1 minute per 2 seconds)");
+        _timeScale = Config.Bind("General", "TimeScale", 60,
+            "The speed of time in the game(60 = 1 in game minute per second & 30 = 1 minute per 2 seconds)");
         _timeInside = Config.Bind("General", "TimeInside", false, "Whether time passes inside buildings");
         // Plugin startup logic
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        
+
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
     }
 
@@ -59,19 +59,45 @@ public class Plugin : BasePlugin
         {
             __instance.TimeScale = _timeScale.Value;
         }
-    
-    
+
+
         [HarmonyPatch(typeof(GameController), "PostSucceededChangeField")]
         [HarmonyPostfix]
         public static void AreaPatch(GameController __instance, uint __0)
         {
-            if(_timeInside.Value) return;
-            //__0 == area id
-            // Spawn name is usually not updated and uses the area before its name
-            if(_areas.Contains(__0))
+            if (_timeInside.Value) return;
+
+            if (_areas.Contains(__0))
+            {
+                if (DateManager.Instance.IsPlay()) return;
                 DateManager.Instance.Play();
+            }
             else
+            {
+                if (!DateManager.Instance.IsPlay()) return;
                 DateManager.Instance.Pause();
+            }
+        }
+
+        [HarmonyPatch(typeof(DateManager), "Play")]
+        [HarmonyPostfix]
+        public static void TimeInsidePatch(DateManager __instance)
+        {
+            if (_timeInside.Value) return;
+
+            var areaid = GameController.Instance.FM.currentFieldId;
+
+
+            if (_areas.Contains(areaid))
+            {
+                if (__instance.IsPlay()) return;
+                __instance.Play();
+            }
+            else
+            {
+                if (!__instance.IsPlay()) return; 
+                __instance.Pause();
+            }
         }
     }
 }
